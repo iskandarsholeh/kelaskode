ebbe06c9ffdc70cede135c8fa2423b216c32edba
Iskandar Sholeh <79823292+iskandarsholeh@users.noreply.github.com>
2021-04-30 20:55:32 +0700

    kelaskode

---
+++ admin/header.php
@@ -0,0 +1,80 @@
+<title>Dashboard | Admin</title>
+<nav class="navbar navbar-default navbar-fixed-top">
+            <div class="brand">
+                <a href="index.php"><img src="assets/img/logo.png" alt="Klorofil Logo" class="img-responsive logo"></a>
+            </div>
+            <div class="container-fluid">
+                <div class="navbar-btn">
+                    <button type="button" class="btn-toggle-fullwidth"><i class="lnr lnr-arrow-left-circle"></i></button>
+                </div>
+                <div id="navbar-menu">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="assets/img/user.png" class="img-circle" alt="Avatar"> <span>Admin</span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="page-profile.php"><i class="lnr lnr-user"></i> <span>My Profile</span></a></li>
+                                <li><a href="changepass.php"><i class="lnr lnr-pencil"></i> <span>Change Password</span></a></li>
+                                <li><a href="../logout"><i class="lnr lnr-exit"></i> <span>Logout</span></a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle icon-menu" data-toggle="dropdown">
+                                <i class="lnr lnr-alarm"></i>
+                                <span class="badge bg-danger">5</span>
+                            </a>
+
+
+                            <ul class="dropdown-menu notifications">
+                                <li><a href="#" class="notification-item"><span class="dot bg-warning"></span>System space is almost full</a></li>
+                                <li><a href="#" class="notification-item"><span class="dot bg-danger"></span>You have 9 unfinished tasks</a></li>
+                                <li><a href="#" class="notification-item"><span class="dot bg-success"></span>Monthly report is available</a></li>
+                                <li><a href="#" class="notification-item"><span class="dot bg-warning"></span>Weekly meeting in 1 hour</a></li>
+                                <li><a href="#" class="notification-item"><span class="dot bg-success"></span>Your request has been approved</a></li>
+                                <li><a href="#" class="more">See all notifications</a></li>
+                            </ul>
+                        </li>
+
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Basic Use</a></li>
+                            <li><a href="#">Working With Data</a></li>
+                            <li><a href="#">Security</a></li>
+                            <li><a href="#">Troubleshooting</a></li>
+                        </ul>
+                        </li>
+
+                        <!-- <li>
+							<a class="update-pro" href="https://www.themeineed.com/downloads/klorofil-pro-bootstrap-admin-dashboard-template/?utm_source=klorofil&utm_medium=template&utm_campaign=KlorofilPro" title="Upgrade to Pro" target="_blank"><i class="fa fa-rocket"></i> <span>UPGRADE TO PRO</span></a>
+						</li> -->
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div id="sidebar-nav" class="sidebar">
+            <div class="sidebar-scroll">
+                <nav>
+                    <ul class="nav">
+                        <li><a href="index.php" <?php if($thisPage == "Home") echo "class='active'"; ?>><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
+                        <li>
+                            <a href="#subHome" data-toggle="collapse" <?php if($thisPage == "About") echo "class='active'"; ?>><i class="lnr lnr-file-empty"></i> <span>Home</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
+                            <div id="subHome" class="collapse ">
+                                <ul class="nav">
+                                    <li><a href="about.php" <?php if($thisPage == "About") echo "class='active'"; ?>>About Us</a></li>
+                                    <li><a href="team.php" <?php if($thisPage == "Team") echo "class='active'"; ?>>Team</a></li>
+                                    <li><a href="price.php" <?php if($thisPage == "Price") echo "class='active'"; ?>>Price</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#subUser" data-toggle="collapse" <?php if($thisPage == "Kelas") echo "class='active'"; ?>><i class="lnr lnr-user"></i> <span>User</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
+                            <div id="subUser" class="collapse ">
+                                <ul class="nav">
+                                    <li><a href="user.php" <?php if($thisPage == "User") echo "class='active'"; ?>>users</a></li>
+                                    <li><a href="kelas.php" <?php if($thisPage == "Kelas") echo "class='active'"; ?>>Kelas</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
---
+++ home/changepass.php
@@ -0,0 +1,168 @@
+<!doctype html>
+<html lang="en">
+
+<?php 
+$thisPage = "";
+    include 'header.php';
+    
+    ?>
+        <!-- MAIN -->
+        <div class="main">
+            <!-- MAIN CONTENT -->
+            <div class="main-content">
+                <div class="container-fluid">
+                    <h3 class="page-title">New Password</h3>
+                    <div class="panel panel-headline demo-icons">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Edit Password</h3>
+                        </div>
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input type="text" class="form-control" name="jenis" required="required">
+                            </div>
+                            <div class="form-group">
+                                <label>Password Baru</label>
+                                <input type="text" class="form-control" name="benefit" required="required">
+                            </div>
+                            <div class="form-group">
+                                <label>Ulangi Password Baru</label>
+                                <input type="text" class="form-control" name="harg" required="required">
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class=" btn btn-primary ">Edit</button>
+                </div>
+            </div>
+        </div>
+        <!-- END MAIN CONTENT -->
+    </div>
+    <!-- END MAIN -->
+    <div class="clearfix"></div>
+    <footer>
+        <div class="container-fluid">
+            <p class="copyright">&copy; 2017 <a href="https://www.themeineed.com" target="_blank">Theme I Need</a>. All Rights Reserved.</p>
+        </div>
+    </footer>
+    </div>
+
+    <div id="myModal" class="modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+                </div>
+                <div class="modal-body">
+
+                    <!-- <div class="form-group">
+                                    <label>Username</label>
+                                    <input type="text" class="form-control" name="username" required="required">
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" class="form-control" name="password" required="required">
+                                </div> -->
+                    <div class="row">
+
+                        <form action="#" method="post">
+                            <div class="col-md-12">
+                                <center>
+                                    <h3>Edit User</h3>
+                                </center>
+                                <div class="form-group">
+                                    <label>Nama</label>
+                                    <input type="text" class="form-control" name="jenis" required="required">
+                                </div>
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input type="text" class="form-control" name="benefit" required="required">
+                                </div>
+                                <div class="form-group">
+                                    <label>Alamat</label>
+                                    <input type="text" class="form-control" name="harg" required="required">
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="text" class="form-control" name="harg" required="required">
+                                </div>
+                            </div>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class=" btn btn-primary ">Edit</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div id="mymodal" class="modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+                </div>
+                <div class="modal-body">
+
+                    <!-- <div class="form-group">
+                                        <label>Username</label>
+                                        <input type="text" class="form-control" name="username" required="required">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Password</label>
+                                        <input type="password" class="form-control" name="password" required="required">
+                                    </div> -->
+                    <div class="row">
+
+                        <form action="#" method="post">
+                            <div class="col-md-12">
+                                <center>
+                                    <h3>Price</h3>
+                                </center>
+                                <div class="form-group">
+                                    <label>Jenis</label>
+                                    <input type="text" class="form-control" name="jenis" required="required">
+                                </div>
+                                <div class="form-group">
+                                    <label>Benefit</label>
+                                    <input type="text" class="form-control" name="benefit" required="required">
+                                </div>
+                                <div class="form-group">
+                                    <label>Harga</label>
+                                    <input type="text" class="form-control" name="harg" required="required">
+                                </div>
+
+                            </div>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class=" btn btn-primary ">Edit</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- END WRAPPER -->
+        <!-- Javascript -->
+        <script src="assets/vendor/jquery/jquery.min.js"></script>
+        <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+        <script src="assets/scripts/klorofil-common.js"></script>
+</body>
+
+</html>
